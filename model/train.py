7647a01ffdc86307b9a8118faf618de30b6ac613
heramb-analytics <93883195+heramb-analytics@users.noreply.github.com>
2025-02-26 00:51:36 +0530

    Update train.py

--- model/train.py
+++ model/train.py
@@ -1,23 +1,26 @@
 import pandas as pd
+import numpy as np
 import mlflow
-import mlflow.sklearn
 import joblib
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
 import os
+import mlflow.sklearn
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+
 
 # Load dataset
 data = pd.read_csv("./data/spam.csv")
 X = data["sms"]
 y = data["label"]
 
-# Split data
+# Split the data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Define model pipelineimport os
+# Defining the model pipeline
+import os
 import pandas as pd
 import mlflow
 import joblib
@@ -37,7 +40,7 @@ X = data["sms"]
 y = data["label"]
 
 # Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=48)
 
 # Define model pipeline
 model_pipeline = Pipeline([
@@ -45,7 +48,7 @@ model_pipeline = Pipeline([
     ("classifier", MultinomialNB())
 ])
 
-# Train model
+# Train the model
 with mlflow.start_run():
     model_pipeline.fit(X_train, y_train)
     preds = model_pipeline.predict(X_test)
@@ -53,7 +56,7 @@ with mlflow.start_run():
     mlflow.log_metric("accuracy", acc)
     mlflow.sklearn.log_model(model_pipeline, "model")
 
-# Save model
+# Save the model
 model_path = "./model/spam_classifier_v1.pkl"
 joblib.dump(model_pipeline, model_path)
 
